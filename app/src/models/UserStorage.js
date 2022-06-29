27aeb07e9f5d3cfd9e86b8836310b441e37edc86
pyola4981 <pyola4981@naver.com>
2022-06-29 23:20:12 +0900

    Feat: User 모델 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -8,6 +8,7 @@ class UserStorage {
     };
 
     static getUsers(...fields) {
+        console.log(fields);
         const users = this.#users;
         const newUsers = fields.reduce((newUsers, field) => {
             if (users.hasOwnProperty(field)) {
@@ -16,6 +17,17 @@ class UserStorage {
             return newUsers;
         }, {});
         return newUsers;
+    }
+
+    static getUserInfo(id) {
+        const users = this.#users;
+        const idx = users.id.indexOf(id);
+        const userInfo = Object.keys(users).reduce((newUser, info) => {
+            newUser[info] = users[info][idx];
+            return newUser;
+        }, {});
+
+        return userInfo;
     }
 }
 
