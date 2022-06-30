32e73d6f47fbbbac9c1787c4e90ea27e3070b861
pyola4981 <pyola4981@naver.com>
2022-06-30 16:15:21 +0900

    Feat: 회원가입 요청 구현

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -1,18 +1,23 @@
 "use strict";
 
 const id = document.querySelector("#id"),
+    name = document.querySelector("#name"),
     psword = document.querySelector("#psword"),
-    loginBtn = document.querySelector("#button");
+    confirmPsword = document.querySelector("#confirm-psword"),
+    registerBtn = document.querySelector("#button");
+console.log("hello register");
+registerBtn.addEventListener("click",register);
 
-loginBtn.addEventListener("click",login);
-
-function login() {
+function register() {
     const req = {
         id: id.value,
+        name: name.value,
         psword: psword.value,
+        confirmPsword: confirmPsword.value,
     };
+    console.log(req);
 
-    fetch("/login", {
+    fetch("/register", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
@@ -22,12 +27,12 @@ function login() {
         .then((res) => res.json())
         .then((res) => {
             if (res.success) {
-                location.href = "/";
+                location.href = "/login";
             }else {
                 alert(res.msg);
             }
         })
         .catch((err) => {
-            console.error("로그인 중 에러 발생");
+            console.error("회원가입 중 에러 발생");
         });
 }
